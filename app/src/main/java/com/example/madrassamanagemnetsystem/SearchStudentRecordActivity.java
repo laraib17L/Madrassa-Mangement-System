72f124d631ed30b28a17783a0a2c88232742a1c9
laraib17L <125170688+laraib17L@users.noreply.github.com>
2023-06-18 03:34:18 -0700

    commit 19

--- app/src/main/java/com/example/madrassamanagemnetsystem/SearchStudentRecordActivity.java
+++ app/src/main/java/com/example/madrassamanagemnetsystem/SearchStudentRecordActivity.java
@@ -1,14 +1,44 @@
 package com.example.madrassamanagemnetsystem;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class SearchStudentRecordActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchStudentActivity extends AppCompatActivity {
+
+    private EditText searchEditText;
+    private RecyclerView recyclerView;
+    private StudentAdapter studentAdapter;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_student_record);
+        setContentView(R.layout.activity_search_student);
+
+        searchEditText = findViewById(R.id.searchEditText);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        Button searchButton = findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchCriteria = searchEditText.getText().toString();
+
+                // Retrieve student records based on search criteria using DatabaseHelper
+                databaseHelper = new DatabaseHelper(SearchStudentActivity.this);
+                List<Student> studentList = databaseHelper.searchStudents(searchCriteria);
+
+                // Set up RecyclerViewAdapter and display the search results
+                studentAdapter = new StudentAdapter(studentList);
+                recyclerView.setAdapter(studentAdapter);
+            }
+        });
     }
 }
